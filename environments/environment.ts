aaee5c7709cb15e8fcd8a1d3e21b84c07526cd4b
yemman <43826395+yemman@users.noreply.github.com>
2026-03-09 19:34:57 +0200

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  proxyUrl: 'https://proxy-request-492842723997.europe-west1.run.app', 
   apiKey: '' // Leave empty; GCP will provide identity
 };
