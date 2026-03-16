b9b0a7e37a53407bd3bd7e5d2732e3972c42641e
yannay.h.i <yannay.h.i@gmail.com>
2026-03-16 13:56:39 +0000

    hot fix

--- environments/environment.dev.ts
+++ environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     proxyUrl: 'https://proxy-request-492842723997.europe-west1.run.app', 
-    apiKey: '***REMOVED***' 
+    apiKey: '' 
   };
   
